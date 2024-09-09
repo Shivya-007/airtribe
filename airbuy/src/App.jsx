288673a1a435166d6b5caac0570d7d35308e13fe
Shivya-007 <167630322+Shivya-007@users.noreply.github.com>
2024-09-10 00:53:02 +0530

    Update App.jsx

--- airbuy/src/App.jsx
+++ airbuy/src/App.jsx
@@ -3,7 +3,18 @@ import Load from './assets/images/spin.gif'
 import './App.css';
 import appFirebase from './Firebase'
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import AppRouter from './router/router';
+//import AppRouter from './router/router';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from "./Components/Home";
+import Lists from "./Components/Lists";
+import Signin from "./Components/Signin";
+import Signup from "./Components/Signup";
+import ProductPage from "./Components/ProductPage";
+import Cart from "./Components/Cart";
+import Payment from "./Components/Payment";
+import Profile from "./Components/Profile";
+import Orders from "./Components/Orders";
+import Error from "./Components/Error";
 
 const auth = getAuth(appFirebase);
 
@@ -33,7 +44,24 @@ const App = () => {
   }
 
   return (
-    <AppRouter user={true} />
+    <BrowserRouter>
+            <Routes>
+                <Route path="/" element={user ? <Navigate to="/home" /> : <Signin />} />
+                <Route path="/signup" element={user ? <Navigate to="/home" /> : <Signup />} />
+                {user && (
+                    <>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/wishlists" element={<Lists />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/payment" element={<Payment />} />
+                        <Route path="/orders" element={<Orders />} />
+                        <Route path="/account" element={<Profile />} />
+                        <Route path="/product/:id" element={<ProductPage />} />
+                        <Route path="*" element={<Error />} />
+                    </>
+                )}
+            </Routes>
+        </BrowserRouter>
   )
 }
 
